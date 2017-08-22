ddecc64e208b6e51c1f95f1ad9e91d425a8e0561
JT Jobe <jjobe@tunecore.com>
2017-08-22 12:55:52 -0400

    i

--- lib/headlines.ex
+++ lib/headlines.ex
@@ -3,22 +3,37 @@ defmodule Headlines do
   def nyt do
     response = HTTPoison.get! "https://www.nytimes.com"
     get_links(response.body)
+    |> extract_from_links
+    |> write_to_csv
   end
 
-  def cnn do 
-   response = HTTPoison.get! "http://www.cnn.com"
-   get_links(response.body)
+  def cnn do
+    response = HTTPoison.get! "http://www.cnn.com"
+    get_links(response.body)
+    |> extract_from_links
+    |> write_to_csv
   end
 
-  def link_titles(links) do 
+  def get_links(html) do
+    Floki.find(html, "a")
+  end
+
+  def extract_from_links(links) do
     Enum.map(links, fn(link) ->
-      headline = elem(link, 2)
-      IO.inspect headline
+      title = List.first(elem(link, 2))
+      url   = elem(List.first(elem(link, 1)), 1)
+      [title, url]
     end)
   end
 
-  defp get_links(html) do
-    Floki.find(html, "a")
+  def write_to_csv(links) do
+    File.open("tmp/nyt.txt", [:write, :utf8], fn(file) ->
+      Enum.map(links, fn(link) ->
+        # if Enum.count(link) == 1 do
+          IO.write(file, CSVLixir.write_row(link))
+        # end
+      end)
+    end)
   end
 
 end
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Headlines.Mixfile do
     [
       app: :headlines,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.3",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
